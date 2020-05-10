a36b0862df51051690d2df87a6586ee3737a7b2b
taielmartinez <taielmartinezgort@gmail.com>
2020-05-10 09:36:03 -0300

    ignore

--- api/v1/src/db_connect.php
+++ api/v1/src/db_connect.php
@@ -1,5 +1,13 @@
 <?php
 
-PRIVATE
+error_reporting(E_ERROR | E_PARSE);
+
+  $conn = new mysqli("localhost","u562328342_php","basededatosrevueltoderadio","u562328342_revuelto"); // server
+  
+  $conn=mysqli_connect("localhost","u562328342_php","basededatosrevueltoderadio","u562328342_revuelto");
+
+   if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
 
 ?>
