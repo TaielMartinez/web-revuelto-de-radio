6fd010f4dd72cfd1b4377248ebf3b2f221ba0eec
taielmartinez <taielmartinezgort@gmail.com>
2020-05-11 10:22:38 -0300

    pagina "discod"

--- disco/disco.js
+++ disco/disco.js
@@ -1,27 +1,32 @@
 let url = window.location.href 
 let id = url.split("?e=")[1];
 
-console.log(id)
+console.log(id) 
 
+if(id == undefined) {
+    console.log("sin id")
+    window.location.href = "../discos"
+} else {
+    $.ajax({
+        url: "../api/v1/discos/",
+        method: "POST",
+        data: {id : id}
+    }).done(function(data) {
+        data = JSON.parse(data)
+        console.log(data)
+        //$('.nuevo_img_discos').attr("src", data);
+    
+        $('.titulo').text(data[0].titulo)
+        $('.subtitulo').text(data[0].subtitulo)
+        $('.fecha').text(data[0].fecha)
+        $('.parrafo').text(data[0].parrafo)
+        $('.titulo').text(data[0].titulo)
+        $('.youtube').attr("href", data[0].youtube)
+        $('.spotify').attr("href", data[0].spotify)
+        $('.foto').attr("src", data[0].foto);
+    
+        if(!data[0].youtube) $('.youtube').hide()
+        if(!data[0].spotify) $('.spotify').hide()
+    });
+}
 
-$.ajax({
-    url: "../api/v1/discos/",
-    method: "POST",
-    data: {id : id}
-}).done(function(data) {
-    data = JSON.parse(data)
-    console.log(data)
-    //$('.nuevo_img_discos').attr("src", data);
-
-    $('.titulo').text(data[0].titulo)
-    $('.subtitulo').text(data[0].subtitulo)
-    $('.fecha').text(data[0].fecha)
-    $('.parrafo').text(data[0].parrafo)
-    $('.titulo').text(data[0].titulo)
-    $('.youtube').attr("href", data[0].youtube)
-    $('.spotify').attr("href", data[0].spotify)
-    $('.foto').attr("src", data[0].foto);
-
-    if(!data[0].youtube) $('.youtube').hide()
-    if(!data[0].spotify) $('.spotify').hide()
-});
